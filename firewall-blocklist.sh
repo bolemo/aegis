c4201565d89a7cae08fa3729bd0e8abf81151da7
bolemo <grvg@free.fr>
2020-04-08 10:59:59 +0200

    Update firewall-blocklist.sh

--- firewall-blocklist.sh
+++ firewall-blocklist.sh
@@ -47,7 +47,7 @@ clean() {
 }
 
 set_ipset() {
-  [ -r $IP_LIST ] || { >&2 echo "$IP_LIST not readable!"; return; }
+  [ -r $IP_LIST ] || { >&2 echo "$IP_LIST not readable!"; exit 1; }
 
   ipset -! create $IPSET_TMP hash:net
   ipset flush $IPSET_TMP > /dev/null
@@ -70,16 +70,21 @@ set_ipset() {
 }
 
 update_iplist() {
-  [ -r $SRC_LIST ] || { >&2 echo "$SRC_LIST not readable!"; return; }
+  [ -r $SRC_LIST ] || { >&2 echo "$SRC_LIST not readable!"; exit 1; }
 
   :>"$TMP_FILE"
   # Process each source url
   [ $VERBOSE ] && echo "Downloading lists defined in $SRC_LIST"
   [ $VERBOSE ] && WGET_OPTS='-qO- --show-progress' || WGET_OPTS='-qO-'
+  _NBOK=0; _TOT=0;
   grep -v "^[[:space:]*\#]" "$SRC_LIST" | while read -r URL; do
-    [ $VERBOSE ] && echo "$URL"
+    _TOT=$((_TOT+1))
+    [ $VERBOSE ] && echo "$_TOT) $URL"
     wget $WGET_OPTS "$URL" | grep '^[0-9]' | sed -e 's/;.*//' >> "$TMP_FILE"
+    [ $? = 0 ] && _NBOK=$((_NBOK+1)) || >&2 echo "Wget error code ${?}! Could not download $URL"
   done
+  [ $_NBOK = 0 ] && { >&2 echo "Could not get any list!"; rm "$TMP_FILE"; exit 1; }
+  [ $_NBOK = $_TOT ] || >&2 echo "Downloaded only $_NBOK / $_TOT list(s)!"
   [ $VERBOSE ] && echo "Removing duplicates..."
   sort "$TMP_FILE" | uniq > "$IP_LIST"
   rm "$TMP_FILE"
