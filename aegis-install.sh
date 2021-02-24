fd40cc10168db9edcd73634447f5c4cbb961c22b
hELLO wORLD <grvg@free.fr>
2021-02-24 01:15:00 +0100

    Update aegis-install.sh

--- aegis-install.sh
+++ aegis-install.sh
@@ -10,8 +10,9 @@ ifconfig ppp0 && WAN_IF='ppp0' || WAN_IF="$(/bin/nvram get wan_ifname)"
 WAN_IP="$(/usr/sbin/ip -4 addr show $WAN_IF|/usr/bin/awk 'NR==2 {print substr($2,0,index($2, "/")-1);exit}')"
 
 _dlinfo() { # to know how many people are downloading this script
-   /usr/bin/curl --interface $WAN_IF -H 'Content-Type: application/json' -H "Authorization: Bearer 1a3mmidk3rg2j1xv6t82ak65up1yht5dambypyh1ze7xhbw7941r" -X POST "https://aegis.goatcounter.com/api/v0/count" \
-                 --data '{"no_sessions": true, "hits": [{"path": "install/'$VERS'", "title": "'$VERS'", "ip": "'$WAN_IP'", "ref": "'$RT_MOD$([ "$CHOICE" ] && echo "/ext" || echo "/int")'"}]}' &
+  [ "$CHOICE" ] && REF2=/ext || REF2=/int
+  /usr/bin/curl --interface $WAN_IF -H 'Content-Type: application/json' -H "Authorization: Bearer 1a3mmidk3rg2j1xv6t82ak65up1yht5dambypyh1ze7xhbw7941r" -X POST "https://aegis.goatcounter.com/api/v0/count" \
+                --data '{"no_sessions": true, "hits": [{"path": "install/'$VERS'", "title": "'$VERS'", "ip": "'$WAN_IP'", "ref": "'$RT_MOD$REF2'"}]}' &
 } >/dev/null 2>&1
 
 ask_yn() {
