7f75c49efdd4e422013785a7500d5b8b41787330
hELLO wORLD <grvg@free.fr>
2021-02-26 09:22:50 +0100

    Update aegis-install.sh

--- aegis-install.sh
+++ aegis-install.sh
@@ -6,7 +6,6 @@ AEGIS_SRC_URL="$AEGIS_REPO/aegis.sources"
 SELF_PATH="$(pwd -P)"
 WGET_PATH="/usr/bin/wget"
 ifconfig ppp0 && WAN_IF='ppp0' || WAN_IF="$(/bin/nvram get wan_ifname)"
-# WAN_IP="$(/usr/sbin/ip -4 addr show $WAN_IF|/usr/bin/awk 'NR==2 {print substr($2,0,index($2, "/")-1);exit}')"
 
 _dlinfo() { # to know how many people are downloading this script
   TITLE="$1" IP="$(/usr/bin/curl --interface $WAN_IF 'ifconfig.me/ip')" REF1="$(cat /module_name)"; [ "$CHOICE" ] && REF2=ext || REF2=int
