2584612e5f977f077115ea1a64e3520137df42e2
hELLO wORLD <grvg@free.fr>
2021-05-31 09:50:02 +0200

    Update pre-upgrade.sh

--- pre-upgrade.sh
+++ pre-upgrade.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
+# Delete log prior to version 1.7.10
+if grep -qF 'PHYSIN=' /var/log/log-aegis; then {
+  /opt/bolemo/scripts/aegis down
+  /sbin/uci -c /opt/bolemo/etc/config/ delete aegis.log.len
+  /bin/rm -f /var/log/log-aegis; } 2>/dev/null
+fi
 echo '- pre-upgrade script: done!'
