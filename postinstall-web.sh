8fddc9630735a3f8b7967e0fdb26123f31ebac31
hELLO wORLD <grvg@free.fr>
2021-05-31 10:35:46 +0200

    Update postinstall-web.sh

--- postinstall-web.sh
+++ postinstall-web.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-REPO="$(/sbin/uci -qc /opt/bolemo/etc/config get aegis.repo)"
-[ -z "$REPO" ] && REPO='stable'
+[ -z "$1" ] && REPO='stable' || REPO="$1"
 GIT_DIR="https://raw.githubusercontent.com/bolemo/aegis/$REPO"
 DAT_DIR='/opt/bolemo/www/aegis_data'
 _getMDFile() {
